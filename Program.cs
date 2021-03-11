880f542793dd2e95c355943d0e2f16c43fa2db7e
sanjana k <sanjanarthi@gmail.com>
2021-03-11 20:48:22 +0530

    "Modified in regex" UC2

--- Program.cs
+++ Program.cs
@@ -8,13 +8,15 @@ namespace RegexPractice
         static void Main(string[] args)
         {
             string firstName = "Sanjana";
-            string pattern = @"[A-Z]\w{2,}";
+            string pattern = @"^[A-Z]\w{2,}";
             var result = Regex.IsMatch(firstName, pattern);
             Console.WriteLine("First name result : " + result);
 
             string lastName = "Karthi";
             result = Regex.IsMatch(lastName, pattern);
             Console.WriteLine("Last name result : " + result);
+
+            
 
 
         }
